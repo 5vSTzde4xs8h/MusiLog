e0aa5054dc8a37599caa4c6c78a47477772d6408
5vSTzde4xs8h <124944815+5vSTzde4xs8h@users.noreply.github.com>
2024-03-17 09:54:25 -0700

    Update TrackMetadataTest

--- app/src/test/java/com/jvn/musilog/data/TrackMetadataTest.java
+++ app/src/test/java/com/jvn/musilog/data/TrackMetadataTest.java
@@ -18,12 +18,13 @@ public class TrackMetadataTest {
    */
   @Test
   public void testFromSpotify() {
+    // https://open.spotify.com/track/2tmuRm2ROZ0vHEhutI2Hlp
     TrackMetadata spotifyMetadata =
         TrackMetadata.fromMusicSource(MusicSource.Spotify, "2tmuRm2ROZ0vHEhutI2Hlp");
 
-    assertEquals(spotifyMetadata.getTitle(), "Mane Mane Psychotropic");
-    assertEquals(spotifyMetadata.getArtistLine(), "Kairikibear");
-    assertNotEquals(spotifyMetadata.getCoverArtUrl(), "");
+    assertEquals("Mane Mane Psychotropic", spotifyMetadata.getTitle());
+    assertEquals("Kairikibear", spotifyMetadata.getArtistLine());
+    assertNotEquals("", spotifyMetadata.getCoverArtUrl());
   }
 
   /**
@@ -35,9 +36,9 @@ public class TrackMetadataTest {
   public void testFromSpotifyBadId() {
     TrackMetadata spotifyMetadata = TrackMetadata.fromMusicSource(MusicSource.Spotify, "badId");
 
-    assertEquals(spotifyMetadata.getTitle(), "");
-    assertEquals(spotifyMetadata.getArtistLine(), "");
-    assertEquals(spotifyMetadata.getCoverArtUrl(), "");
+    assertEquals("", spotifyMetadata.getTitle());
+    assertEquals("", spotifyMetadata.getArtistLine());
+    assertEquals("", spotifyMetadata.getCoverArtUrl());
   }
 
   /**
@@ -47,15 +48,16 @@ public class TrackMetadataTest {
    */
   @Test
   public void testFromYouTube() {
+    // https://www.youtube.com/watch?v=Dun11cIEo9s
     TrackMetadata youTubeMetadata =
         TrackMetadata.fromMusicSource(MusicSource.YouTube, "Dun11cIEo9s");
 
     assertEquals(
-        youTubeMetadata.getTitle(),
-        "Neru - potatoになっていく (Becoming Potatoes) feat. Kagamine Rin & Kagamine Len");
+        "Neru - potatoになっていく (Becoming Potatoes) feat. Kagamine Rin & Kagamine Len",
+        youTubeMetadata.getTitle());
 
-    assertEquals(youTubeMetadata.getArtistLine(), "Neru OFFICIAL");
-    assertNotEquals(youTubeMetadata.getCoverArtUrl(), "");
+    assertEquals("Neru OFFICIAL", youTubeMetadata.getArtistLine());
+    assertNotEquals("", youTubeMetadata.getCoverArtUrl());
   }
 
   /**
@@ -67,9 +69,9 @@ public class TrackMetadataTest {
   public void testFromYouTubeBadId() {
     TrackMetadata youTubeMetadata = TrackMetadata.fromMusicSource(MusicSource.YouTube, "badId");
 
-    assertEquals(youTubeMetadata.getTitle(), "");
-    assertEquals(youTubeMetadata.getArtistLine(), "");
-    assertEquals(youTubeMetadata.getCoverArtUrl(), "");
+    assertEquals("", youTubeMetadata.getTitle());
+    assertEquals("", youTubeMetadata.getArtistLine());
+    assertEquals("", youTubeMetadata.getCoverArtUrl());
   }
 
   /**
@@ -80,8 +82,8 @@ public class TrackMetadataTest {
   public void testFromUnknown() {
     TrackMetadata unknownMetadata = TrackMetadata.fromMusicSource(MusicSource.Unknown, "");
 
-    assertEquals(unknownMetadata.getTitle(), "");
-    assertEquals(unknownMetadata.getArtistLine(), "");
-    assertEquals(unknownMetadata.getCoverArtUrl(), "");
+    assertEquals("", unknownMetadata.getTitle());
+    assertEquals("", unknownMetadata.getArtistLine());
+    assertEquals("", unknownMetadata.getCoverArtUrl());
   }
 }
