e7d38092467cf63d529fd6bbc8e973eadd4b33ee
5vSTzde4xs8h <124944815+5vSTzde4xs8h@users.noreply.github.com>
2024-03-30 08:20:25 -0700

    Update TrackMetadata behaviour

--- app/src/test/java/com/jvn/musilog/data/TrackMetadataTest.java
+++ app/src/test/java/com/jvn/musilog/data/TrackMetadataTest.java
@@ -11,6 +11,16 @@ import org.junit.Test;
  * @since 2024-03-16
  */
 public class TrackMetadataTest {
+  /** Tests that the qualified constructor works as intended. */
+  @Test
+  public void testQualifiedConstructor() {
+    TrackMetadata metadata = new TrackMetadata("Title", "Artists", "https://www.example.org/");
+
+    assertEquals("Title", metadata.getTitle());
+    assertEquals("Artists", metadata.getArtistLine());
+    assertEquals("https://www.example.org/", metadata.getCoverArtUrl());
+  }
+
   /**
    * Tests that metadata can be retrieved for a Spotify music track. This test depends on external
    * factors such as network and Spotify service availability, and assumes that all such factors do
@@ -22,23 +32,15 @@ public class TrackMetadataTest {
     TrackMetadata spotifyMetadata =
         TrackMetadata.fromMusicSource(MusicSource.Spotify, "2tmuRm2ROZ0vHEhutI2Hlp");
 
+    TrackMetadata spotifyMetadataBadId =
+        TrackMetadata.fromMusicSource(MusicSource.Spotify, "badId");
+
+    assertNotNull(spotifyMetadata);
+    assertNull(spotifyMetadataBadId);
+
     assertEquals("Mane Mane Psychotropic", spotifyMetadata.getTitle());
     assertEquals("Kairikibear", spotifyMetadata.getArtistLine());
     assertNotEquals("", spotifyMetadata.getCoverArtUrl());
-  }
-
-  /**
-   * Tests that metadata cannot be retrieved for a bad Spotify music track ID. This test depends on
-   * external factors such as network and Spotify service availability, and assumes that all such
-   * factors do not fail.
-   */
-  @Test
-  public void testFromSpotifyBadId() {
-    TrackMetadata spotifyMetadata = TrackMetadata.fromMusicSource(MusicSource.Spotify, "badId");
-
-    assertEquals("", spotifyMetadata.getTitle());
-    assertEquals("", spotifyMetadata.getArtistLine());
-    assertEquals("", spotifyMetadata.getCoverArtUrl());
   }
 
   /**
@@ -52,6 +54,12 @@ public class TrackMetadataTest {
     TrackMetadata youTubeMetadata =
         TrackMetadata.fromMusicSource(MusicSource.YouTube, "Dun11cIEo9s");
 
+    TrackMetadata youTubeMetadataBadId =
+        TrackMetadata.fromMusicSource(MusicSource.YouTube, "badId");
+
+    assertNotNull(youTubeMetadata);
+    assertNull(youTubeMetadataBadId);
+
     assertEquals(
         "Neru - potatoになっていく (Becoming Potatoes) feat. Kagamine Rin & Kagamine Len",
         youTubeMetadata.getTitle());
@@ -60,30 +68,10 @@ public class TrackMetadataTest {
     assertNotEquals("", youTubeMetadata.getCoverArtUrl());
   }
 
-  /**
-   * Tests that metadata cannot be retrieved for a bad YouTube music track ID. This test depends on
-   * external factors such as network and YouTube service availability, and assumes that all such
-   * factors do not fail.
-   */
-  @Test
-  public void testFromYouTubeBadId() {
-    TrackMetadata youTubeMetadata = TrackMetadata.fromMusicSource(MusicSource.YouTube, "badId");
-
-    assertEquals("", youTubeMetadata.getTitle());
-    assertEquals("", youTubeMetadata.getArtistLine());
-    assertEquals("", youTubeMetadata.getCoverArtUrl());
-  }
-
-  /**
-   * Tests that attempting to retrieve metadata from an unknown source will return an empty {@link
-   * TrackMetadata}.
-   */
+  /** Tests that attempting to retrieve metadata from an unknown source will return {@code null}. */
   @Test
   public void testFromUnknown() {
     TrackMetadata unknownMetadata = TrackMetadata.fromMusicSource(MusicSource.Unknown, "");
-
-    assertEquals("", unknownMetadata.getTitle());
-    assertEquals("", unknownMetadata.getArtistLine());
-    assertEquals("", unknownMetadata.getCoverArtUrl());
+    assertNull(unknownMetadata);
   }
 }
